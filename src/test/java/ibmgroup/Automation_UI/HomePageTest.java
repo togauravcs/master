610c9dc52809ea8c3d40933767eb20107859d08b
GauravSingh <GauravSingh@DESKTOP-EC491I0>
2020-03-03 01:53:24 +0530

    First Commit and Push

---
+++ src/test/java/ibmgroup/Automation_UI/HomePageTest.java
@@ -0,0 +1,29 @@
+package ibmgroup.Automation_UI;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class HomePageTest {
+	
+
+	@Test
+	public void openGmail() throws InterruptedException
+	{
+		System.out.println("Test Case 1");
+		
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\GauravSingh\\Desktop\\Selen\\Automation_UI\\Drivers\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("http://www.google.com");
+		
+		WebElement searchBox = driver.findElement(By.xpath("//input[@title='Search']"));
+		searchBox.click();
+		searchBox.sendKeys("Gmail");
+		searchBox.sendKeys(Keys.ENTER);
+		Thread.sleep(2000);
+		driver.close();
+	}
+}
